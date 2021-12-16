65f4104caffde9f7991dd52446ec3547342bad61
TheMachineDK <59374945+TheMachineDK@users.noreply.github.com>
2021-12-16 18:24:05 +0100

    Add files via upload

---
+++ remove_doors/configs/serverconfig.lua
@@ -0,0 +1,12 @@
+ConfigACS = {}
+
+ConfigACS.ModelsLogWebhook = "https://discord.com/api/webhooks/807615147551621131/78xgnqkGpnXnvRnS2R-BTzGBcSFCL5tvsmkQjF9qwtjPEomViGXX9zvgVajIB9eU73-5"
+ConfigACS.ExplosionLogWebhook = "https://discord.com/api/webhooks/807615147551621131/78xgnqkGpnXnvRnS2R-BTzGBcSFCL5tvsmkQjF9qwtjPEomViGXX9zvgVajIB9eU73-5"
+
+ConfigACS.LogBanWebhook = "https://discord.com/api/webhooks/807615147551621131/78xgnqkGpnXnvRnS2R-BTzGBcSFCL5tvsmkQjF9qwtjPEomViGXX9zvgVajIB9eU73-5" --YOUR BANS/LOGS WEBHOOK
+ConfigACS.AntiVPNWebhook = "https://discord.com/api/webhooks/807615147551621131/78xgnqkGpnXnvRnS2R-BTzGBcSFCL5tvsmkQjF9qwtjPEomViGXX9zvgVajIB9eU73-5" -- ANTIVPN WEBHOOK
+
+ConfigACS.Version = "v4.0" -- DONT TOUCH THIS! (if you change this the anticheats is not gonna work)
+
+--//Permission System//-- DO NOT TOUCH IF YOU DON'T KNOW WHAT ARE U DOING 
+ConfigACS.Bypass = {"polarisacowner","polarisacadministrator", "polarisacmoderator"}
